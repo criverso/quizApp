5929683db7b2ab52e5e6d33a7bb51e2b3aae555a
Jon Gomez <jgomez8558@gmail.com>
2018-07-12 11:08:11 -0500

    Add Services to Components
    Stage 1 to link services.

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class QuizService {
+  // Place all varibles here
+  description = "I am working in the service!";
 
   constructor() { }
 }
