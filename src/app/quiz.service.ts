49f0f0e67435f3b90b9b0bfb592dad76ea57b889
Jon Gomez <jgomez8558@gmail.com>
2018-07-13 15:37:38 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -7,8 +7,8 @@ import { Router } from '@angular/router';
 export class QuizService {
 
   // Gets the value from user and adds it to the total and moves to next question.
-  goNextQuestion(result) {
-    this.totalResult = this.totalResult + result;
+  goNextQuestion() {
+    this.totalResult = this.totalResult + this.result;
     
     if (this.currentQuestion < 10 ) {
       this.currentQuestion++;
@@ -33,6 +33,12 @@ export class QuizService {
       this.router.navigate([`/`]);
     }
 
+  }
+
+  calculateResult() {
+    for(let i = 0; i < this.questions.length; i++) {
+      this.totalResult += this.questions[i].selectedAnswer;
+    }
   }
   // Checks to see what is the final results and display the results.
   getResult(totalResult){
@@ -66,274 +72,234 @@ export class QuizService {
   questions = [
     // Question 1
     {
-      id: 1,
       question: "Which choice best describes you?",
+      selectedAnswer: 0,
       choices: [ 
         {
           answer: "A. Teach",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Problem Solving",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Debate",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Help People",
           value: 4, 
-          selection: false
         }
       ], 
     },
     // Question 2
     {
-      id: 2,
       question: "What is your best quality?",
+      selectedAnswer: 1,
       choices: [ 
         {
           answer: "A. Outgoing Personality",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Drive and Focus",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Good Looks",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Kindness",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 3
     {
-      id: 3,
       question: "What kind of music do you like?",
+      selectedAnswer: 2,
       choices: [ 
         {
           answer: "A. Country",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Metal",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Pop music",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Classical",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 4
     {
-      id: 4,
       question: "Are you an introvert or an extrovert?",
+      selectedAnswer: 3,
       choices: [ 
         {
           answer: "A. Introvert",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Exrovert",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Both",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. I don't know",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 5
     {
-      id: 5,
       question: "What is your favorite subject?",
+      selectedAnswer: 4,
       choices: [ 
         {
           answer: "A. English",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Math",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Government",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Biology",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 6
     // Insert a picture in choices
     {
-      id: 6,
       question: "Which vehicle do you like best",
+      selectedAnswer: 5,
       choices: [ 
         {
           answer: "A. Ford Focus",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Challenger",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Mercedes",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Ferari",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 7
     {
-      id: 7,
       question: "What can't you live without?",
+      selectedAnswer: 6,
       choices: [ 
         {
           answer: "A. Books",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Computer",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Music",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Phone",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 8
     // Insert Picture in choices
     { 
-      id: 8,
       question: "Which style suits you best?",
+      selectedAnswer: 7,
       choices: [ 
         {
           answer: "A. slacks and tennis",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. T-shirt and jeans",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Suit",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Hawaiian Shirt",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 9
     {
-      id: 9,
       question: "Which activity do you like doing the most?",
+      selectedAnswer: 8,
       choices: [ 
         {
           answer: "A. Arts and Crafts",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Skateboarding",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Golf",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Fishing",
           value: 4, 
-          selection: false
         }
       ],
     },
     // Question 10
     // Insert Picture in choices
     { 
-      id: 10,
       question: "Which meal do you like the most?",
+      selectedAnswer: 9,
       choices: [ 
         {
           answer: "A. Tacos",
           value: 1, 
-          selection: false
         },
         {
           answer: "B. Pizza",
           value: 2, 
-          selection: false
         },
         {
           answer: "C. Goose",
           value: 3, 
-          selection: false
         },
         {
           answer: "D. Caviar",
           value: 4, 
-          selection: false
         }
       ]
     }
